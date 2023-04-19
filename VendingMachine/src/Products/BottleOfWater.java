2480d6a94cffa75eb798b4b26181aec999fc410a
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-04-19 14:08:05 +0300

    Добавили файл BottleOfWater

---
+++ VendingMachine/src/Products/BottleOfWater.java
@@ -0,0 +1,34 @@
+package Products;
+
+public class BottleOfWater extends Product {
+
+    private int volume;
+
+    public BottleOfWater(String name, double price, int volume)
+    {
+        super(name, price);
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+        /**
+     * переопределение вывода продукта
+     */
+    @Override
+    public String toString()
+    {
+        return  "Product{" +
+        "name='" + super.getName() + '\'' +
+        ", cost=" + super.getPrice() +
+        ", volume=" + volume +
+        '}';
+    }
+    
+}
