8717340c3fdc9bd7dbeee4f989e1c2bbe6ea8493
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-04-24 17:01:26 +0300

    Добавили самодокументацию в файл iMarketBehaviour

--- SuperMarket/src/Interfaces/iMarketBehaviour.java
+++ SuperMarket/src/Interfaces/iMarketBehaviour.java
@@ -1,3 +1,6 @@
+/**
+* Этот интерфейс определяет поведение рынка, на котором актеры могут принимать участие.
+*/
 package Interfaces;
 
 import java.util.List;
@@ -5,7 +8,18 @@ import java.util.List;
 import Classes.Actor;
 
 public interface iMarketBehaviour {
+/**
+* Регистрирует покупателя в супермаркете.
+* @param actor объект поведения покупателя, который будет зарегистрирован в супермаркете
+*/
     void acceptToMarket(iActorBehaviour actor);
+/**
+ * Удаляет покупателя из супермаркета.
+ * @param actors список покупателей, которые будут удалены из супермаркета
+ */
     void releaseFromMarket(List<Actor> actors);
+ /**
+ * Обновляет состояние супермаркета.
+ */ 
     void update();    
 }
--- SuperMarket/src/Interfaces/iQueueBehaviour.java
+++ SuperMarket/src/Interfaces/iQueueBehaviour.java
@@ -1,10 +1,26 @@
+/**
+Этот интерфейс определяет поведение очереди в супермаркете.
+*/
 package Interfaces;
 
 import Classes.Actor;
 
 public interface iQueueBehaviour {
+/**
+* Добавление покупателя в очередь.
+* @param actor объект поведения покупателя, который будет добавлен в очередь
+*/
     void takeInQueue(iActorBehaviour actor);
+/**
+* Удаление первого покупателя из очереди.
+*/
     void releaseFromQueue();
+/**
+* Система принимает заказ от первого покупателя в очереди.
+*/
     void takeOrder();
+/**
+* Заказ первого покупателя в очереди.
+*/
     void giveOrder();
 }
