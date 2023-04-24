264bb10351f815d005151fa779d8c5cc94350d27
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-04-24 16:36:14 +0300

    Продолжили добавление самодокументации к части файлов

--- SuperMarket/src/Classes/OrdinaryClient.java
+++ SuperMarket/src/Classes/OrdinaryClient.java
@@ -24,27 +24,37 @@ public class OrdinaryClient extends Actor {
     public String getName() {        
         return super.name;
     }
-
+/**
+*  Метод, который устанавливает значение логической переменной isMakeOrder, которая указывает, делает ли клиент заказ.
+*/
     @Override
     public boolean isMakeOrder() {
         return super.isMakeOrder;
     }
-
+/**
+* Метод, который возвращает значение логической переменной isTakeOrder, которая указывает, получил ли клиент заказ.
+*/
     @Override
     public boolean isTakeOrder() {
         return super.isTakeOrder;
     }
-
+/**
+* Метод, который устанавливает значение логической переменной isMakeOrder, которая указывает, делает ли клиент заказ. 
+*/
     @Override
     public void setMakeOrder(boolean makeOrder) {
        super.isMakeOrder = makeOrder;
     }
-
+/**
+* Метод, который устанавливает значение логической переменной isTakeOrder, которая указывает, получил ли клиент заказ. 
+*/
     @Override
     public void setTakeOrder(boolean pickUpOrder) {
        super.isTakeOrder = pickUpOrder;   
     }
-
+/*
+*  Метод, который возвращает объект Actor, используемый для идентификации клиента.
+*/
     @Override
     public Actor getActor() {
         return this;
