13a0b8e3d47559e506d3f3b42c2f63cc6a9fbe4b
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-05-23 18:14:02 +0300

    Добавили файлы проекта

---
+++ Solid/SpeedCalculation.java
@@ -0,0 +1,34 @@
+package Solid;
+/*
+ * 2) Переписать код SpeedCalculation в соответствии с Open-Closed Principle:
+public class SpeedCalculation {
+public double calculateAllowedSpeed(Vehicle vehicle) {
+if (vehicle.getType().equalsIgnoreCase("Car")) {
+return vehicle.getMaxSpeed() * 0.8;
+} else if (vehicle.getType().equalsIgnoreCase("Bus")) {
+return vehicle.getMaxSpeed() * 0.6;
+}
+    return 0.0;
+}
+}
+public class Vehicle {
+int maxSpeed;
+String type;
+public Vehicle(int maxSpeed, String type) {
+this.maxSpeed = maxSpeed;
+this.type = type;
+}
+public int getMaxSpeed() {
+return this.maxSpeed;
+}
+public String getType() {
+return this.type;
+}
+}
+
+Подсказка: создайте два дополнительных класса Car и Bus(наследников Vehicle), напишите метод calculateAllowedSpeed(). 
+Использование этого метода позволит сделать класс SpeedCalculation соответствующим OCP
+ */
+public class SpeedCalculation {
+    
+}
