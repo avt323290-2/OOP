fdd2335039f5ac07d6dae3f75d15fbe2b2cf385e
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-05-23 19:51:17 +0300

    Добавили решение второй задачи

--- Solid/SpeedCalculation.java
+++ Solid/SpeedCalculation.java
@@ -29,6 +29,112 @@ return this.type;
 Подсказка: создайте два дополнительных класса Car и Bus(наследников Vehicle), напишите метод calculateAllowedSpeed(). 
 Использование этого метода позволит сделать класс SpeedCalculation соответствующим OCP
  */
+/**
+ * Абстрактный класс, представляющий транспортное средство.
+ */
+/**
+ * Абстрактный класс, представляющий транспортное средство.
+ */
+public abstract class Vehicle {
+    int maxSpeed;
+    String type;
+
+    /**
+     * Конструктор класса Vehicle.
+     *
+     * @param maxSpeed максимальная скорость транспортного средства
+     * @param type     тип транспортного средства
+     */
+    public Vehicle(int maxSpeed, String type) {
+        this.maxSpeed = maxSpeed;
+        this.type = type;
+    }
+
+    /**
+     * Получить максимальную скорость транспортного средства.
+     *
+     * @return максимальная скорость
+     */
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    /**
+     * Получить тип транспортного средства.
+     *
+     * @return тип транспортного средства
+     */
+    public String getType() {
+        return this.type;
+    }
+
+    /**
+     * Рассчитать разрешенную скорость движения транспортного средства.
+     *
+     * @return разрешенная скорость
+     */
+    public abstract double calculateAllowedSpeed();
+}
+
+/**
+ * Класс, представляющий автомобиль.
+ */
+public class Car extends Vehicle {
+    /**
+     * Конструктор класса Car.
+     *
+     * @param maxSpeed максимальная скорость автомобиля
+     */
+    public Car(int maxSpeed) {
+        super(maxSpeed, "Car");
+    }
+
+    /**
+     * Рассчитать разрешенную скорость движения автомобиля.
+     *
+     * @return разрешенная скорость
+     */
+    @Override
+    public double calculateAllowedSpeed() {
+        return getMaxSpeed() * 0.8;
+    }
+}
+
+/**
+ * Класс, представляющий автобус.
+ */
+public class Bus extends Vehicle {
+    /**
+     * Конструктор класса Bus.
+     *
+     * @param maxSpeed максимальная скорость автобуса
+     */
+    public Bus(int maxSpeed) {
+        super(maxSpeed, "Bus");
+    }
+
+    /**
+     * Рассчитать разрешенную скорость движения автобуса.
+     *
+     * @return разрешенная скорость
+     */
+    @Override
+    public double calculateAllowedSpeed() {
+        return getMaxSpeed() * 0.6;
+    }
+}
+
+/**
+ * Класс, отвечающий за расчет разрешенной скорости транспортного средства.
+ */
 public class SpeedCalculation {
-    
+    /**
+     * Рассчитать разрешенную скорость движения транспортного средства.
+     *
+     * @param vehicle транспортное средство
+     * @return разрешенная скорость
+     */
+    public double calculateAllowedSpeed(Vehicle vehicle) {
+        return vehicle.calculateAllowedSpeed();
+    }
 }
