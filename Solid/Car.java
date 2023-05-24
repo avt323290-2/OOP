09e59cf7be2864e153a7cab4cc3515de72876900
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-05-24 17:39:27 +0300

    Добавили решение пятой задачи

--- Solid/Car.java
+++ Solid/Car.java
@@ -17,6 +17,59 @@ public void start() {
  */
 package Solid;
 
+/**
+ * Интерфейс Engine представляет двигатель.
+ */
+public interface Engine {
+    /**
+     * Запускает двигатель.
+     */
+    void start();
+}
+
+/**
+ * Класс Car представляет автомобиль.
+ */
 public class Car {
-    
+    private Engine engine;
+
+    /**
+     * Создает экземпляр класса Car с указанным двигателем.
+     *
+     * @param engine двигатель автомобиля
+     */
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    /**
+     * Запускает автомобиль.
+     */
+    public void start() {
+        this.engine.start();
+    }
+}
+
+/**
+ * Класс PetrolEngine представляет бензиновый двигатель.
+ */
+public class PetrolEngine implements Engine {
+    /**
+     * Запускает бензиновый двигатель.
+     */
+    public void start() {
+        // Код для запуска бензинового двигателя
+    }
+}
+
+/**
+ * Класс DieselEngine представляет дизельный двигатель.
+ */
+public class DieselEngine implements Engine {
+    /**
+     * Запускает дизельный двигатель.
+     */
+    public void start() {
+        // Код для запуска дизельного двигателя
+    }
 }
