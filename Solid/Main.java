a2db0f9076f2a76fe77254ab47aa5bafa7a4b739
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-05-24 17:17:20 +0300

    Добавили решение четвертой  задачи

--- Solid/Main.java
+++ Solid/Main.java
@@ -26,3 +26,84 @@ super.height = height;
 }
 }
  */
+package Solid;
+
+/**
+ * Класс Rectangle представляет прямоугольную фигуру.
+ */
+public class Rectangle {
+    protected int width;
+    protected int height;
+
+    /**
+     * Устанавливает ширину прямоугольника.
+     *
+     * @param width ширина прямоугольника
+     */
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    /**
+     * Устанавливает высоту прямоугольника.
+     *
+     * @param height высота прямоугольника
+     */
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    /**
+     * Вычисляет и возвращает площадь прямоугольника.
+     *
+     * @return площадь прямоугольника
+     */
+    public int area() {
+        return this.width * this.height;
+    }
+}
+
+/**
+ * Класс Square представляет квадратную фигуру.
+ */
+public class Square extends Rectangle {
+    /**
+     * Устанавливает ширину и высоту квадрата равными переданному значению.
+     *
+     * @param sideLength длина стороны квадрата
+     */
+    @Override
+    public void setWidth(int sideLength) {
+        super.setWidth(sideLength);
+        super.setHeight(sideLength);
+    }
+
+    /**
+     * Устанавливает ширину и высоту квадрата равными переданному значению.
+     *
+     * @param sideLength длина стороны квадрата
+     */
+    @Override
+    public void setHeight(int sideLength) {
+        super.setWidth(sideLength);
+        super.setHeight(sideLength);
+    }
+}
+
+/**
+ * Главный класс, демонстрирующий использование классов Rectangle и Square.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Создание прямоугольника
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(5);
+        rectangle.setHeight(10);
+        System.out.println("Площадь прямоугольника: " + rectangle.area());
+
+        // Создание квадрата
+        Square square = new Square();
+        square.setWidth(5);
+        System.out.println("Площадь квадрата: " + square.area());
+    }
+}
