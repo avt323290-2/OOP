82bec6113092b738d66d560ae0685780da3924d2
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-05-31 13:03:00 +0300

    Добавили содержимое файла FileLogger.java

--- ComplexCalculate/src/logger/FileLogger.java
+++ ComplexCalculate/src/logger/FileLogger.java
@@ -1,5 +1,23 @@
-package ComplexCalculate.src.logger;
+package logger;
 
-public class FileLogger {
-    
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileLogger implements Logger {
+    private String fileName;
+
+    public FileLogger(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void log(String message) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            writer.write("[FileLogger] " + message);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
