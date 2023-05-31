d8766d9dbff648f9c04b2cb185dae786eea0920b
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-05-31 12:59:56 +0300

    Добавили содержимое файла ComplexCalculator.java

--- ComplexCalculate/src/calculator/ComplexCalculator.java
+++ ComplexCalculate/src/calculator/ComplexCalculator.java
@@ -1,5 +1,33 @@
-package ComplexCalculate.src.calculator;
+package calculator;
 
-public class ComplexCalculator {
-    
+import complex.ComplexNumber;
+import logger.Logger;
+
+public class ComplexCalculator implements Calculator {
+    private Logger logger;
+
+    public ComplexCalculator(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Override
+    public ComplexNumber add(ComplexNumber num1, ComplexNumber num2) {
+        ComplexNumber result = num1.add(num2);
+        logger.log("Performed addition: " + num1 + " + " + num2 + " = " + result);
+        return result;
+    }
+
+    @Override
+    public ComplexNumber multiply(ComplexNumber num1, ComplexNumber num2) {
+        ComplexNumber result = num1.multiply(num2);
+        logger.log("Performed multiplication: " + num1 + " * " + num2 + " = " + result);
+        return result;
+    }
+
+    @Override
+    public ComplexNumber divide(ComplexNumber num1, ComplexNumber num2) {
+        ComplexNumber result = num1.divide(num2);
+        logger.log("Performed division: " + num1 + " / " + num2 + " = " + result);
+        return result;
+    }
 }
