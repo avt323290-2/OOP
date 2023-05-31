a50950ded25da08fb1d09393aad21fc9f496e5a4
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-05-31 13:24:46 +0300

    Дополнили Main.java

--- ComplexCalculate/src/Main.java
+++ ComplexCalculate/src/Main.java
@@ -2,6 +2,8 @@ import calculator.*;
 import complex.*;
 import logger.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Create a logger (choose either ConsoleLogger or FileLogger)
@@ -10,17 +12,34 @@ public class Main {
         // Create an instance of the complex calculator
         Calculator calculator = new ComplexCalculator(logger);
 
-        // Perform some calculations
-        ComplexNumber num1 = new ComplexNumber(2.5, 3.2);
-        ComplexNumber num2 = new ComplexNumber(1.8, -2.7);
+        // Create a Scanner object to read input from the user
+        Scanner scanner = new Scanner(System.in);
 
+        // Prompt the user to enter the first complex number
+        System.out.println("Введите действительную часть первого комплексного числа:");
+        double real1 = scanner.nextDouble();
+        System.out.println("Введите мнимую часть первого комплексного числа:");
+        double imaginary1 = scanner.nextDouble();
+        ComplexNumber num1 = new ComplexNumber(real1, imaginary1);
+
+        // Prompt the user to enter the second complex number
+        System.out.println("Введите действительную часть второго комплексного числа:");
+        double real2 = scanner.nextDouble();
+        System.out.println("Введите мнимую часть первого комплексного числа:");
+        double imaginary2 = scanner.nextDouble();
+        ComplexNumber num2 = new ComplexNumber(real2, imaginary2);
+
+        // Perform calculations
         ComplexNumber sum = calculator.add(num1, num2);
         ComplexNumber product = calculator.multiply(num1, num2);
         ComplexNumber quotient = calculator.divide(num1, num2);
 
         // Print the results
-        System.out.println("Sum: " + sum);
-        System.out.println("Product: " + product);
-        System.out.println("Quotient: " + quotient);
+        System.out.println("Сумма: " + sum);
+        System.out.println("Произведение: " + product);
+        System.out.println("ЧАстное: " + quotient);
+
+        // Close the Scanner object
+        scanner.close();
     }
 }
--- ComplexCalculate/src/calculator/ComplexCalculator.java
+++ ComplexCalculate/src/calculator/ComplexCalculator.java
@@ -13,21 +13,21 @@ public class ComplexCalculator implements Calculator {
     @Override
     public ComplexNumber add(ComplexNumber num1, ComplexNumber num2) {
         ComplexNumber result = num1.add(num2);
-        logger.log("Performed addition: " + num1 + " + " + num2 + " = " + result);
+        logger.log("Выполненное сложение: " + num1 + " + " + num2 + " = " + result);
         return result;
     }
 
     @Override
     public ComplexNumber multiply(ComplexNumber num1, ComplexNumber num2) {
         ComplexNumber result = num1.multiply(num2);
-        logger.log("Performed multiplication: " + num1 + " * " + num2 + " = " + result);
+        logger.log("Выполненное умножение: " + num1 + " * " + num2 + " = " + result);
         return result;
     }
 
     @Override
     public ComplexNumber divide(ComplexNumber num1, ComplexNumber num2) {
         ComplexNumber result = num1.divide(num2);
-        logger.log("Performed division: " + num1 + " / " + num2 + " = " + result);
+        logger.log("Выполненное деление: " + num1 + " / " + num2 + " = " + result);
         return result;
     }
 }
