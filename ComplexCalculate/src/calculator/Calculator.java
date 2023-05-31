c16b05bd8c1f6d12ec84eb755c04cf6130ecfc2d
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-05-31 13:26:54 +0300

    Дополнили автодокументацию в файл calculator.java

--- ComplexCalculate/src/calculator/Calculator.java
+++ ComplexCalculate/src/calculator/Calculator.java
@@ -2,8 +2,34 @@ package calculator;
 
 import complex.ComplexNumber;
 
+/**
+ * Интерфейс калькулятора для работы с комплексными числами.
+ */
 public interface Calculator {
+    /**
+     * Складывает два комплексных числа.
+     *
+     * @param num1 Первое комплексное число.
+     * @param num2 Второе комплексное число.
+     * @return Результат сложения комплексных чисел.
+     */
     ComplexNumber add(ComplexNumber num1, ComplexNumber num2);
+
+    /**
+     * Умножает два комплексных числа.
+     *
+     * @param num1 Первое комплексное число.
+     * @param num2 Второе комплексное число.
+     * @return Результат умножения комплексных чисел.
+     */
     ComplexNumber multiply(ComplexNumber num1, ComplexNumber num2);
+
+    /**
+     * Делит одно комплексное число на другое.
+     *
+     * @param num1 Делимое комплексное число.
+     * @param num2 Делитель комплексное число.
+     * @return Результат деления комплексных чисел.
+     */
     ComplexNumber divide(ComplexNumber num1, ComplexNumber num2);
 }
