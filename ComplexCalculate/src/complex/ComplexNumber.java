5654b3d82659c36d2699658717ca0cd2d5d8a52c
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-05-31 12:51:29 +0300

    Добавили содержимое файла ComplexNumber.java

--- ComplexCalculate/src/complex/ComplexNumber.java
+++ ComplexCalculate/src/complex/ComplexNumber.java
@@ -1,5 +1,47 @@
-package ComplexCalculate.src.complex;
+package complex;
 
 public class ComplexNumber {
-    
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public ComplexNumber add(ComplexNumber other) {
+        double sumReal = this.real + other.real;
+        double sumImaginary = this.imaginary + other.imaginary;
+        return new ComplexNumber(sumReal, sumImaginary);
+    }
+
+    public ComplexNumber multiply(ComplexNumber other) {
+        double productReal = this.real * other.real - this.imaginary * other.imaginary;
+        double productImaginary = this.real * other.imaginary + this.imaginary * other.real;
+        return new ComplexNumber(productReal, productImaginary);
+    }
+
+    public ComplexNumber divide(ComplexNumber other) {
+        double divisor = other.real * other.real + other.imaginary * other.imaginary;
+        double quotientReal = (this.real * other.real + this.imaginary * other.imaginary) / divisor;
+        double quotientImaginary = (this.imaginary * other.real - this.real * other.imaginary) / divisor;
+        return new ComplexNumber(quotientReal, quotientImaginary);
+    }
+
+    @Override
+    public String toString() {
+        if (imaginary >= 0) {
+            return real + " + " + imaginary + "i";
+        } else {
+            return real + " - " + Math.abs(imaginary) + "i";
+        }
+    }
 }
